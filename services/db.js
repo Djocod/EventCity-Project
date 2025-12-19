d3844647fc2733fd3022066ba4e089adb13bd335
Djocod <jordan.francoual@hotmail.com>
2025-12-19 01:46:48 +0100

    add modification searchArtist

--- services/db.js
+++ services/db.js
@@ -1,18 +1,11 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql");
 const config = require("../config");
 
 async function query(sql, params) {
   const connection = await mysql.createConnection(config.database);
 
-  try {
-    const [results] = await connection.execute(sql, params);
-    return results;
-  } catch (error) {
-    console.error("❌ Erreur SQL:", error.message);
-    throw error;
-  } finally {
-    await connection.end();
-  }
+  const [results] = await connection.execute(sql, params);
+  return results;
 }
 
 module.exports = {
